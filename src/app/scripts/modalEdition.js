df4c2221548f6dc402e953320265013a4b475029
Valentina Cedano <valentina.cedano57@gmail.com>
2024-03-09 18:20:41 -0500

    Creación Home aside maquetación

---
+++ src/app/scripts/modalEdition.js
@@ -0,0 +1,19 @@
+const cartButton = document.getElementById("btnCard");
+
+cartButton.addEventListener("click", () => {
+    console.log("Ay me tocaste");
+    
+});
+
+console.log(cartButton);
+const modal = document.querySelector(".modalCart");
+const closeButton = document.getElementById("closeModal");
+
+
+const toggleModal = (button, modal) => {
+    
+};
+
+toggleModal(cartButton,modal);
+toggleModal(closeButton,modal);
+
