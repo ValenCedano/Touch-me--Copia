404197c9e1bed42772ab1d5c772342e83aba354b
Andrea Guerrero <guerreroaanathalia@gmail.com>
2024-03-15 00:01:19 -0500

    se agrego las alertas al registro, Registro listo

--- src/app/scripts/index.js
+++ src/app/scripts/index.js
@@ -4,9 +4,15 @@ import { getDataForm } from "../modules/getDataform.js";
 import {validateDataRegister} from "../modules/validateDataRegister.js";
 import endpoints from "../services/data.js";
 import {sendUserRegister,getUser} from "../services/userServices.js";
+import { alertModal,valuesAlert } from '../modules/alert.js';
 
-// document.addEventListener("DOMContentLoaded", () => {
-    let  contentMain= document.getElementById("content");
+document.addEventListener("DOMContentLoaded", () => {
+    const contentMain= document.getElementById("content");
+    chargeLogin(contentMain);
+   
+});
+
+const chargeLogin=(contentMain)=>{
     contentMain.innerHTML=`
     <div class="container__form init">                  
         <h1> Sing in</h1>        
@@ -32,6 +38,11 @@ import {sendUserRegister,getUser} from "../services/userServices.js";
         </p>
     </div>
     `;
+    const register=document.getElementById("register");
+    register.addEventListener("click", (event) => {
+        event.preventDefault();
+        chargeRegister(contentMain)
+    });
     const form = document.getElementById("myForm");
     form.addEventListener("submit", (event) => {
         event.preventDefault();
@@ -41,68 +52,78 @@ import {sendUserRegister,getUser} from "../services/userServices.js";
         // console.log("validation",validated);
         // contentMain.innerHTML=`<p>Home</p>`
     });
-    const register=document.getElementById("register");
-    register.addEventListener("click", (event) => {
-        event.preventDefault();
-        contentMain.innerHTML=`
-        <div class="container__form">                  
-            <h1> Register</h1>        
-            <form id="form" >
-                <div class="form-group">
-                    <label for="userName">Name:</label>
-                    <input type="text" id="userName" name="name">
-                    <span id="errorName" class="alert"></span>
-                </div>
-                <div class="form-group">
-                    <label for="userImage">Profile picture:</label>
-                    <input type="url" id="userImage" name="image" >
-                    <span id="errorImage" class="alert"></span>
-                </div>
-                <div class="form-group">
-                    <label for="userPhone">Phone:</label>
-                    <input type="number" id="userPhone" name="phone">
-                    <span id="errorPhone" class="alert"></span>
-                </div>
-                <div class="form-group">
-                    <label for="userPassword">Password:</label>
-                    <input type="password" id="userPassword" name="password">
-                    <span id="errorPassword" class="alert"></span>
-                </div>
-                <div class="form-group">
-                    <label for="userInfo">Info:</label>
-                    <textarea id="userInfo" name="info" rows="4" cols="50">
-                    </textarea>
-                    <span id="errorInfo" class="alert"></span>
-                </div>           
-                <div class="form-group">
-                    <button id="button" type="submit">Sing in</button>
-                </div>
-            </form>
-        </div>
-        `;
-        const form = document.getElementById("form");
-        form.addEventListener("submit", async (event) => {
-            event.preventDefault();
-            const data = getDataForm(form);
-            const validated= validateDataRegister(data);
-            if(validated){
-                const existPhone= await getUser(endpoints.getAnUserByPhone(data.phone));
-                if(existPhone){
-                    alert("Phone number is alredy registered")
-                }
-                else{
-                    console.log("puedo hacer post")
-                    data.online=false;
-                    data.info=data.info.trim();
-                    await sendUserRegister(endpoints.users,data);
-                    alert("Successfully registered")
-                    form.reset()
+}
 
-                }
+const chargeRegister= (contentMain)=>{
+    contentMain.innerHTML=`
+    <div class="container__form">                  
+        <h1> Register</h1>        
+        <form id="form" >
+            <div class="form-group">
+                <label for="userName">Name:</label>
+                <input type="text" id="userName" name="name">
+                <span id="errorName" class="alert"></span>
+            </div>
+            <div class="form-group">
+                <label for="userImage">Profile picture:</label>
+                <input type="url" id="userImage" name="image" >
+                <span id="errorImage" class="alert"></span>
+            </div>
+            <div class="form-group">
+                <label for="userPhone">Phone:</label>
+                <input type="number" id="userPhone" name="phone">
+                <span id="errorPhone" class="alert"></span>
+            </div>
+            <div class="form-group">
+                <label for="userPassword">Password:</label>
+                <input type="password" id="userPassword" name="password">
+                <span id="errorPassword" class="alert"></span>
+            </div>
+            <div class="form-group">
+                <label for="userInfo">Info:</label>
+                <textarea id="userInfo" name="info" rows="4" cols="50">
+                </textarea>
+                <span id="errorInfo" class="alert"></span>
+            </div>           
+            <div class="form-group">
+                <button id="button" type="submit">Sing in</button>
+            </div>
+        </form>
+    </div>
+    `;
+    const form = document.getElementById("form");
+    form.addEventListener("submit", async (event) => {
+        event.preventDefault();
+        const data = getDataForm(form);
+        const validated= validateDataRegister(data);
+        if(validated){
+            const existPhone= await getUser(endpoints.getAnUserByPhone(data.phone));
+            if(existPhone){
+                valuesAlert.title="Error registering";
+                valuesAlert.text="Phone number is alredy registered"
+                valuesAlert.icon="error";
+                valuesAlert.showConfirmButton=true;
+                alertModal(valuesAlert);
             }
             else{
-                console.error("Invalid data")
+                data.online=false;
+                data.info=data.info.trim();
+                await sendUserRegister(endpoints.users,data);
+                valuesAlert.icon="success";
+                valuesAlert.timer=2000;
+                valuesAlert.title="Successfully registered";
+                form.reset();
+                setTimeout(function() {
+                    alertModal(valuesAlert);
+                }, 500);
+                setTimeout(function() {
+                    chargeLogin(contentMain);
+                }, 2500);
+
             }
-        });
+        }
+        else{
+            console.error("Invalid data")
+        }
     });
-//  });
+}
